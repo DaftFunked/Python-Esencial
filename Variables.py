357536722becf7d0c481ca4df32d2d83b60ab7fa
Gamaliel MT <gamaliel1.mt@gmail.com>
2025-01-11 06:49:02 -0600

    Actualización de ejercicios

---
+++ Variables.py
@@ -0,0 +1,14 @@
+nombre = "Gamaliel"
+
+edad = 20
+
+estatura = 1.70
+
+positivo = True
+negativo = False
+
+print(type(nombre))
+print(type(edad))
+print(type(estatura))
+print(type(positivo))
+print(type(negativo))
