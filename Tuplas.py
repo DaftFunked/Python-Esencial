254703a70bf08203f90112a656c179733ea48a5c
Gamaliel MT <gamaliel1.mt@gmail.com>
2025-01-12 01:29:38 -0600

    Actualización de ejercicios

---
+++ Tuplas.py
@@ -0,0 +1,10 @@
+lenguajes = ("Python", "C", "C++")
+
+print(lenguajes)
+
+lenguajes = "Python", "C", "C++"
+
+print(lenguajes)
+
+print(lenguajes[0]) # Acceder a un elemento de la tupla a través de su índice
+print(lenguajes[-1]) # Acceder al último elemento de la tupla
